d109a4a9be0476de518db92a4cad62cef93be3d9
elliotchan20 <32104144+elliotchan20@users.noreply.github.com>
2018-08-08 16:39:01 +0800

    Update MainController.java
    updated route

--- src/main/java/lu/lllc/boottest/controller/MainController.java
+++ src/main/java/lu/lllc/boottest/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/route-1")
 	public String hello(){
 		return "hello";
 	}
